967d442b63b242c0c4b63549121d48e7e0c10a88
pabloalaniz <pabloalaniz@culturainteractiva.com>
2023-12-27 22:16:45 -0300

    Readme with path

--- setup.py
+++ setup.py
@@ -1,12 +1,19 @@
+import os
 from setuptools import setup, find_packages
 
+# Obtener la ruta absoluta al directorio donde se encuentra setup.py
+HERE = os.path.abspath(os.path.dirname(__file__))
+
+# Abrir el README.md desde esa ruta
+with open(os.path.join(HERE, 'README.md'), encoding='utf-8') as fh:
+    long_description = fh.read()
 setup(
     name='GSpreadManager',
     version='0.1.5',
     author='PabloAlaniz',
     author_email='pablo@culturainteractiva.com',
     description='Un módulo de Python para gestionar y automatizar tareas en Google Sheets.',
-    long_description=open('README.md').read(),
+    long_description=long_description,
     long_description_content_type='text/markdown',
     url='https://github.com/PabloAlaniz/GSpreadManager',
     packages=find_packages(),
